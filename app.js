6ec18af08f73ff34a6f0c6ad373e14662eae76fc
Michael Yu <fish.0217@gmail.com>
2016-08-29 03:28:54 +0800

    分檔、調整頁面

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-/*原本不能跑的待測試 var app = require('express')();
-var http = require('http');*/
-
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html'); //處理從client送來的連線
 });
 
-//var server = http.createServer(app); //原本不能跑的待測試
+app.get('/printFunction.js', function(req, res){
+    res.sendFile(__dirname + '/printFunction.js'); //處理從client送來的連線
+});
 
-app.get('/',function(request, response){ //我們要處理URL為 "/" 的HTTP GET請求
-    response.end('你好！'); //作出回應
+app.get('/style-sheet.css', function(req, res){
+    res.sendFile(__dirname + '/style-sheet.css'); //處理從client送來的連線
 });
 
 //指定localhost port: 3000
@@ -19,7 +18,15 @@ http.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
 
-/*//原本不能跑的待測試
+/*原本的 var app = require('express')();
+var http = require('http');*/
+
+//var server = http.createServer(app); //原本的
+// app.get('/',function(request, response){ //我們要處理URL為 "/" 的HTTP GET請求
+//     response.end('你好！'); //作出回應
+// });
+
+/*//原本的
 //指定port: 8080
 server.listen(8080,'127.0.0.1',function(){
     console.log('HTTP伺服器在 http://127.0.0.1:8080/ 上運行');
